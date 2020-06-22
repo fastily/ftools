a98c1816b2cf655ae51df25938a821a270ae4117
fastily <fastily@users.noreply.github.com>
2020-06-22 01:32:44 -0700

    updated deploy scripts, normalize ip4 cidrs in ip-range-calc

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-#: Copies contents of out to web server directory
+#: Generates web pages and copies them to web server directory.  Must be run in an environment with Python 3.7+
 #:
 #: PRECONDITION:
 #: 		1) Web server has been started (run "webserver start")
+#:		2) The ftools repository has been cloned into the tool's home directory.
+#:
 #: Author: Fastily
 
-publicHtmlDir="public_html"
-rootPublicHtmlDir=~/"${publicHtmlDir}"
+j2hRoot="${HOME}/j2h"
+j2hActivate="${j2hRoot}/bin/activate"
 
-## Copy public_html
-printf "Copying public_html...\n"
-mkdir -p "${rootPublicHtmlDir}"
+if [ ! -d "$j2hRoot" ]; then
+	python3 -m venv "${j2hRoot}"
+fi
 
-cp -Rf "out"/* "${rootPublicHtmlDir}"/
+source "${j2hActivate}"
+if ! hash jinja2html > /dev/null; then
+	pip3 install jinja2html
+fi
 
-printf "OK\n"
+cd "${HOME}/ftools"
+jinja2html --generate
+cp -Rf out/* "${HOME}/public_html"/
---
+++ startPython3.7Shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#: Convenience script to start a python3.7 shell on toolforge
+#: 
+#: Author: Fastily
+
+webservice --backend=kubernetes python3.7 shell
